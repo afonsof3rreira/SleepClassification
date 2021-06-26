f3f6cf785aedaba186cc57fcfd0e929c22e65741
afonsof3rreira <afonsof3rreira@users.noreply.github.com>
2021-06-26 17:37:54 +0100

    Signal content display added

--- check_all_avail_signals.m
+++ check_all_avail_signals.m
@@ -7,7 +7,6 @@ list_avail_transd = strings(1, 30);
 
 list_avail_signals(1, 1:length(starting_labels)) = starting_labels;
 list_avail_transd(1, 1:length(starting_transds)) = starting_transds;
-% z = list_avail_signals(list_avail_signals(:) ~= "");
 
 for i = 2: size(signal_header, 1)
     temp_labels = convertCharsToStrings(signal_header{i,1}.label);
@@ -27,5 +26,33 @@ end
 
 labels = list_avail_signals(list_avail_signals(:) ~= "");
 transducers = list_avail_transd(list_avail_transd(:) ~= "");
+
+eeg_count = sum(contains(transducers, "EEG"));
+other_transds = transducers(~contains(transducers, "EEG"));
+
+
+nrep_other_transds = unique(other_transds); 
+char_cell = convertStringsToChars(deblank(nrep_other_transds));
+
+nrep_other_transds_count = strings(1, size(nrep_other_transds, 2));
+
+
+all_transducers = {};
+all_transducers{1, 1} = 'EEG';
+for i = 2 : length(nrep_other_transds)+1
+    all_transducers{1, i} = char_cell{i-1};
+end
+
+
+for i = 1: length(nrep_other_transds)
+    temp_transd = nrep_other_transds(1, i);
+    nrep_other_transds_count(1, i) = sum(contains(transducers, nrep_other_transds(1, i)));
+end
+
+total_count = str2double([eeg_count; nrep_other_transds_count(1, :)'])';
+
+disp("The number of signals found for all transducer types is:");
+T = array2table(total_count,'VariableNames',all_transducers,'RowName',{'Count'}); 
+disp(T) 
 
 end
