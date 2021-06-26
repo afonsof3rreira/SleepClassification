183c59a2fa1e6e801a5c8ca01c927f712ad6ec37
afonsof3rreira <afonsof3rreira@users.noreply.github.com>
2021-06-26 16:23:01 +0100

    Added 2 sections for analysing the signal header

--- main.m
+++ main.m
@@ -33,27 +33,27 @@ for i=1:length(FileNames_edf)
 end
 
 %% Save header and signals
-save('.\Normal_dataset\signal_header.mat','signal_header')
+save('./Normal_dataset/signal_header.mat','signal_header')
 
 n1 = signal{1};
-save('.\Normal_dataset\n1.mat', 'n1', '-v7.3')
+save('./Normal_dataset/n1.mat', 'n1', '-v7.3')
 n2 = signal{2};
-save('.\Normal_dataset\n2.mat', 'n2', '-v7.3')
+save('./Normal_dataset/n2.mat', 'n2', '-v7.3')
 n3 = signal{3};
-save('.\Normal_dataset\n3.mat', 'n3', '-v7.3')
+save('./Normal_dataset/n3.mat', 'n3', '-v7.3')
 n5 = signal{4};
-save('.\Normal_dataset\n5.mat', 'n5', '-v7.3')
+save('./Normal_dataset/n5.mat', 'n5', '-v7.3')
 n11 = signal{5};
-save('.\Normal_dataset\n11.mat', 'n11', '-v7.3')
+save('./Normal_dataset/n11.mat', 'n11', '-v7.3')
 
 %% Load signals
 clear all
-load('.\Normal_dataset\signal_header.mat')
-load('.\Normal_dataset\n1.mat')
-load('.\Normal_dataset\n2.mat')
-load('.\Normal_dataset\n3.mat')
-load('.\Normal_dataset\n5.mat')
-load('.\Normal_dataset\n11.mat')
+load('./Normal_dataset/signal_header.mat')
+load('./Normal_dataset/n1.mat')
+load('./Normal_dataset/n2.mat')
+load('./Normal_dataset/n3.mat')
+load('./Normal_dataset/n5.mat')
+load('./Normal_dataset/n11.mat')
 
 %% Read txt
 % turn txts into column vector
