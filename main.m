f04368b0b248b42e7cd3118c928f80696e365fd7
afonsof3rreira <afonsof3rreira@users.noreply.github.com>
2021-06-27 13:14:52 +0100

    Added signal selection

--- main.m
+++ main.m
@@ -60,6 +60,64 @@ load('./Normal_dataset/n3.mat')
 load('./Normal_dataset/n5.mat')
 load('./Normal_dataset/n11.mat')
 
+%% checking all available different signals for a given signal header
+
+[labels, transducers] = check_all_avail_signals(signal_header);
+labels = deblank(labels);
+transducers = deblank(transducers);
+
+%% Selecting signals types common to all patients / datasets
+
+[common_labs, common_transds] = check_common_signals(signal_header);
+
+[~, ~, ib] = intersect(common_labs, signal_header{1, 1}.label);
+n1 = n1(ib(:), :);
+
+[~, ~, ib] = intersect(common_labs, signal_header{2, 1}.label);
+n2 = n2(ib(:), :);
+
+[~, ~, ib] = intersect(common_labs, signal_header{3, 1}.label);
+n3 = n3(ib(:), :);
+
+[~, ~, ib] = intersect(common_labs, signal_header{4, 1}.label);
+n5 = n5(ib(:), :);
+
+[~, ~, ib] = intersect(common_labs, signal_header{5, 1}.label);
+n11 = n11(ib(:), :);
+
+clear ib;
+
+selection_info = [common_labs; common_transds];
+save('./Selected_dataset/selection_info.mat','selection_info')
+disp("selected info saved")
+
+save('./Selected_dataset/signal_header.mat','signal_header')
+disp("header saved")
+
+save('./Selected_dataset/n1.mat', 'n1', '-v7.3')
+disp("n1 saved")
+
+save('./Selected_dataset/n2.mat', 'n2', '-v7.3')
+disp("n2 saved")
+
+save('./Selected_dataset/n3.mat', 'n3', '-v7.3')
+disp("n3 saved")
+
+save('./Selected_dataset/n5.mat', 'n5', '-v7.3')
+disp("n5 saved")
+
+save('./Selected_dataset/n11.mat', 'n11', '-v7.3')
+disp("n11 saved")
+
+%% Load selected signals
+clear all
+load('./Selected_dataset/common_labels.mat')
+load('./Selected_dataset/signal_header.mat')
+load('./Selected_dataset/n1.mat')
+load('./Selected_dataset/n2.mat')
+load('./Selected_dataset/n3.mat')
+load('./Selected_dataset/n5.mat')
+load('./Selected_dataset/n11.mat')
 %% Read txt
 % turn txts into column vector
 sleepstages=[];
@@ -68,33 +126,4 @@ for i=1:length(FileNames_txt)
     sleepstages = [sleepstages;ss];
     disp(i+"/"+length(FileNames_txt)+" done")
 end
-
-%% checking all available different signals for a given signal header
-
-[labels, transducers] = check_all_avail_signals(signal_header);
-labels = deblank(labels);
-transducers = deblank(transducers);
-
-%% Selecting subsignals common in all signals (n1 to n11)
-
-j = 1;
-common_labels = convertCharsToStrings(signal_header{j,1}.label);
-% disp("i = " + num2str(1));
-% disp(common_labels);
-    
-for i = j + 1: size(signal_header, 1)
-    temp_labels = convertCharsToStrings(signal_header{i,1}.label);
-%     disp("i = " + num2str(i));
-%     disp(temp_labels);
-    common_labels = intersect(common_labels, temp_labels);
-end
-
-labels_example = convertCharsToStrings(signal_header{2,1}.label);
-[C,~,ib] = intersect(common_labels, labels_example);
-
-common_transducers = convertCharsToStrings(signal_header{2,1}.transducer);
-
-common_transducers = common_transducers(1, ib(:, 1));
-
-disp(sum(count(common_transducers, "EEG")));
 
