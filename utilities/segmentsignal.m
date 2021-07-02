0d21819d6f7a8e413d22891437250b4d87dbf0e0
Ana Matoso <anamatoso92@gmail.com>
2021-07-02 12:50:45 +0100

    Update segmentsignal.m
    para eliminar as epocas finais que nao sao de 30s

--- utilities/segmentsignal.m
+++ utilities/segmentsignal.m
@@ -6,4 +6,9 @@ for i=1:epochnumber
     epochs(i,:)=signal(i*30*fs-30*fs+1:i*30*fs);
 end
 epochs= epochs(any(epochs,2),:);
+lastnumber=epochs(end,end);
+while lastnumber==0
+    epochs=epochs(1:end-1,:);
+    lastnumber=epochs(end,end);
+end
 end
