0e17114d307acb6f4b7db66e59c0e33aaa855b1f
Ana Matoso <anamatoso92@gmail.com>
2021-07-03 18:39:35 +0100

    update waves

--- utilities/waves_eeg1.m
+++ utilities/waves_eeg1.m
@@ -1,16 +1,17 @@
 function [percentalpha, percentbeta, percenttheta, percentdelta] = waves_eeg1(EEG, Fsample)
     Hs = spectrum.welch;
     Spectrum = psd(Hs,EEG,'Fs',Fsample);
+    total=trapz(Spectrum.data);
     
-    alpha=trapz(Spectrum.data(9:16));
-    percentalpha= alpha;
+    alpha=trapz(Spectrum.data(8:16));
+    percentalpha= alpha/total;
     
-    beta= trapz(Spectrum.data(17:32));
-    percentbeta=beta;
+    beta= trapz(Spectrum.data(16:32));
+    percentbeta=beta/total;
     
-    theta= trapz(Spectrum.data(5:8));
-    percenttheta= theta;
+    theta= trapz(Spectrum.data(4:8));
+    percenttheta= theta/total;
     
     delta= trapz(Spectrum.data(1:4));
-    percentdelta=delta;
+    percentdelta=delta/total;
 end
--- utilities/waves_eeg2.m
+++ utilities/waves_eeg2.m
@@ -1,4 +1,4 @@
-function [percentalpha, percentbeta, percenttheta, percentdelta,percentgamma] = waves_eeg2(signal, Fs)
+function [percentalpha, percentbeta, percenttheta, percentdelta] = waves_eeg2(signal, Fs)
 
 
 waveletName='db8';
@@ -6,11 +6,8 @@ level=log2(Fs)-2;
 
 % Multilevel 1-D wavelet decomposition for channel 0
 [c0,l0]=wavedec(signal,level,waveletName);
-signalpower = (sum(signal.^2))/length(signal);
+signalpower = (sum(c0.^2))/length(c0);
 % 1-D detail coefficients
-cD1 = detcoef(c0,l0,level-3); %GAMMA
-percentgamma = (sum(cD1.^2))/length(cD1)/signalpower;
-
 cD2 = detcoef(c0,l0,level-2); %BETA
 percentbeta = (sum(cD2.^2))/length(cD2)/signalpower;
 
