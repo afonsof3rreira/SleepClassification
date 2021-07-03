f74419dfbc0027b7b7c6410c28cbb2bc63948189
Ana Matoso <anamatoso92@gmail.com>
2021-07-03 18:44:56 +0100

    Update waves_eeg1.m

--- utilities/waves_eeg1.m
+++ utilities/waves_eeg1.m
@@ -1,17 +1,17 @@
-function [percentalpha, percentbeta, percenttheta, percentdelta] = waves_eeg1(EEG, Fsample)
+function [percentalpha, percentbeta, percenttheta, percentdelta] = waves_eeg1(EEG, sf)
     Hs = spectrum.welch;
-    Spectrum = psd(Hs,EEG,'Fs',Fsample);
+    Spectrum = psd(Hs,EEG,'Fs',sf);
     total=trapz(Spectrum.data);
     
-    alpha=trapz(Spectrum.data(8:16));
+    alpha=trapz(Spectrum.data(8:13));
     percentalpha= alpha/total;
     
-    beta= trapz(Spectrum.data(16:32));
+    beta= trapz(Spectrum.data(18:25));
     percentbeta=beta/total;
     
     theta= trapz(Spectrum.data(4:8));
     percenttheta= theta/total;
     
-    delta= trapz(Spectrum.data(1:4));
+    delta= trapz(Spectrum.data(1:2));
     percentdelta=delta/total;
 end
