d93e9c9025461c76abc46a56b34d3c7f20e891a5
Ana Matoso <anamatoso92@gmail.com>
2021-07-04 16:39:27 +0100

    Create featureextraction.m

---
+++ utilities/featureextraction.m
@@ -0,0 +1,31 @@
+function [features] = featureextraction(EEG1,EEG2,ECG,EMG1,EEG3,HR,EEG4,EOG,EMG2, sfs)
+%featureextraction selects the features of each signal putting them in a vector
+features=[];
+
+%EEG features
+EEG=[EEG1; EEG2; EEG3; EEG4];
+for i=1:4
+    if i<3, sf=sfs(i);
+    elseif i==3, sf=sfs(5);
+    else sf=sfs(7);end
+    eeg=EEG(i,:);deeg=gradient(eeg,1/sf);ddeeg=gradient(deeg,1/sf);
+    l=length(eeg);
+    %calculo das features
+    waves=WavesEEG(eeg, sf);
+    kurt=kurtosis(eeg);
+    sk=skewness(eeg);
+    zc=zerocrossings(eeg); pfd=log10(l)/(log10(l)+log10(l/(l+0.4*zc)));
+    v0=var(eeg);v1=var(deeg);v2=var(ddeeg);
+    hjorth=[(v0^2) v1/v2 sqrt((v2/c1)^2-(v1/v0)^2)];
+    features=[features waves kurt sk pfd hjorth];
+end
+
+%EMG features
+
+
+%EOG features
+
+
+%put all features in a vector
+end
+
