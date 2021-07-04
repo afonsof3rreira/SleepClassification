673391280650be188d06111d2ee81fb5b96b51c2
Ana Matoso <anamatoso92@gmail.com>
2021-07-04 17:52:05 +0100

    Update blinks.m

--- utilities/blinks.m
+++ utilities/blinks.m
@@ -1,4 +1,5 @@
 function [nblinks] = blinks(EOG, sf)
+%yelds the number of blinks detected
 warning('off','all')
 [peaks,~]=findpeaks(EOG,sf,'MinPeakHeight',75,'MinPeakDistance',round(500/sf));
 nblinks=length(peaks);
